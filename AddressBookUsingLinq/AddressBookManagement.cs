605c0a28929b44b1418f83ea95a59292bc59ad31
Bhagi <puppyreddy1997@gmail.com>
2020-12-19 22:08:28 +0530

    [Bhagyalaxmi]ADDEd:Adding new Columns to dataTable

--- AddressBookUsingLinq/AddressBookManagement.cs
+++ AddressBookUsingLinq/AddressBookManagement.cs
@@ -11,6 +11,12 @@ namespace AddressBookUsingLinq
         public DataTable dataTable = new DataTable();
         public AddressBookManagement()
         {
+            DataColumn dataColumn;
+            dataColumn = new DataColumn();
+            dataColumn.DataType = typeof(Int32);
+            dataColumn.ColumnName = "PersonId";
+            dataColumn.AutoIncrement = true;
+            dataTable.Columns.Add(dataColumn);
             dataTable.Columns.Add("FirstName", typeof(string));
             dataTable.Columns.Add("LastName", typeof(string));
             dataTable.Columns.Add("Address", typeof(string));
@@ -19,13 +25,21 @@ namespace AddressBookUsingLinq
             dataTable.Columns.Add("Zip", typeof(double));
             dataTable.Columns.Add("PhoneNumber", typeof(double));
             dataTable.Columns.Add("Email", typeof(string));
+            dataColumn = new DataColumn();
+            dataColumn.DataType = typeof(string);
+            dataColumn.ColumnName = "PersonType";
+            dataColumn.AllowDBNull = false;
+            dataTable.Columns.Add(dataColumn);
+            DataColumn[] PrimaryKeyColumns = new DataColumn[1];
+            PrimaryKeyColumns[0] = dataTable.Columns["PersonID"];
+            dataTable.PrimaryKey = PrimaryKeyColumns;
 
-            dataTable.Rows.Add("Bhagya", "Laxmi", "Nagarkurnool", "MBNR", "TS", 509215, 8410257963, "Bhagi@gmail.com");
-            dataTable.Rows.Add("sravani", "sabbesett", "GandhiNagar", "VijayaWada", "Ap", 509000, 8419874102, "Sravani@gmail.com");
-            dataTable.Rows.Add("TejasWini", "Kulkarni", "Mumbai", "RamNagar", "TS", 509211, 8410257900, "Teju@gmail.com");
-            dataTable.Rows.Add("Rishitha", "Reddy", "PRP", "JCL", "AP", 509201, 8410257321, "Rishitha@gmail.com");
-            dataTable.Rows.Add("SriCharitha", "Reddy", "BPL", "KlKy", "UP", 509206, 8410257123, "Charitha@gmail.com");
-            dataTable.Rows.Add("Navya", "Reddy", "MEP", "KlKy", "UP", 509206, 8410257111, "navya@gmail.com");
+            dataTable.Rows.Add(1,"Bhagya", "Laxmi", "Nagarkurnool", "MBNR", "TS", 509215, 8410257963, "Bhagi@gmail.com","Family");
+            dataTable.Rows.Add(2,"sravani", "sabbesett", "GandhiNagar", "VijayaWada", "Ap", 509000, 8419874102, "Sravani@gmail.com","Friend");
+            dataTable.Rows.Add(3,"TejasWini", "Kulkarni", "Mumbai", "RamNagar", "TS", 509211, 8410257900, "Teju@gmail.com","profession");
+            dataTable.Rows.Add(4,"Rishitha", "Reddy", "PRP", "JCL", "AP", 509201, 8410257321, "Rishitha@gmail.com","Family");
+            dataTable.Rows.Add(5,"SriCharitha", "Reddy", "BPL", "KlKy", "UP", 509206, 8410257123, "Charitha@gmail.com","Family");
+            dataTable.Rows.Add(6,"Navya", "Reddy", "MEP", "KlKy", "UP", 509206, 8410257111, "navya@gmail.com","Friend");
         } 
         public void getAllData()
         {
@@ -103,11 +117,6 @@ namespace AddressBookUsingLinq
                         where person.Field<string>("City").Equals(city)
                         orderby person.Field<string>("FirstName")
                         select person;
-           /* var Records = from record in dataTable.AsEnumerable()
-                          where record.Field<string>("City").Equals(city)
-                          orderby record.Field<string>("FirstName")
-                          select record;*/
-
             foreach (var record in Records)
             {
                 getAllData();
