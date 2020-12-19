355a9e6cd474f995947a0665cd1aa4e2f899581b
Bhagi <puppyreddy1997@gmail.com>
2020-12-19 19:18:42 +0530

    [Bhagyalaxmi]ADDEd:Inserting Values Into the DataTable

--- AddressBookUsingLinq/AddressBookManagement.cs
+++ AddressBookUsingLinq/AddressBookManagement.cs
@@ -19,6 +19,13 @@ namespace AddressBookUsingLinq
             dataTable.Columns.Add("Zip", typeof(double));
             dataTable.Columns.Add("PhoneNumber", typeof(double));
             dataTable.Columns.Add("Email", typeof(string));
+
+            dataTable.Rows.Add("Bhagya", "Laxmi", "Nagarkurnool", "MBNR", "TS", 509215, 8410257963, "Bhagi@gmail.com");
+            dataTable.Rows.Add("sravani", "sabbesett", "GandhiNagar", "VijayaWada", "Ap", 509000, 8419874102, "Sravani@gmail.com");
+            dataTable.Rows.Add("TejasWini", "Kulkarni", "Mumbai", "RamNagar", "TS", 509211, 8410257900, "Teju@gmail.com");
+            dataTable.Rows.Add("Rishitha", "Reddy", "PRP", "JCL", "AP", 509201, 8410257321, "Rishitha@gmail.com");
+            dataTable.Rows.Add("SriCharitha", "Reddy", "BPL", "KlKy", "UP", 509206, 8410257123, "Charitha@gmail.com");
+            dataTable.Rows.Add("Navya", "Reddy", "MEP", "KlKy", "UP", 509206, 8410257111, "navya@gmail.com");
         }        
     }   
 }
