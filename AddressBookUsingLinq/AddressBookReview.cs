aab7139efacf777e099d9820554a70a12d0de50c
Bhagi <puppyreddy1997@gmail.com>
2020-12-19 19:11:06 +0530

    [Bhagyalaxmi]ADDEd:Creating the DataTable

--- AddressBookUsingLinq/AddressBookReview.cs
+++ AddressBookUsingLinq/AddressBookReview.cs
@@ -6,6 +6,13 @@ namespace AddressBookUsingLinq
 {
     public class AddressBookReview
     {
-       
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public double Zip { get; set; }
+        public double PhoneNumber { get; set; }
+        public string Email { get; set; }
     }
 }
